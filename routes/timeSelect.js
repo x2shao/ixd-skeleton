3aa2fa01471ee12e138d5f6b30bd54d9695d816d
lhwlyd <yil623@ucsd.edu>
2018-02-11 14:19:12 -0800

    Time selection UI basics finished

--- routes/timeSelect.js
+++ routes/timeSelect.js
@@ -1,8 +1,10 @@
-
+var dt = new Date();
 /*
  * GET home page.
  */
 
 exports.view = function(req, res){
-  res.render('timeSelect');
+	res.render('timeSelect', {
+		'currentTime':( ("0"+dt.getHours()).slice(-2) + ":" +("0"+dt.getMinutes()).slice(-2))
+	});
 };
