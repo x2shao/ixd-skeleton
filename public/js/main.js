a610cdb777249dc0b9fb873edd273f3db767d2af
Aishma Raghu <airaghu@ucsd.edu>
2018-02-11 19:02:36 -0800

    WizardOfOz Login w/ some amount of UI tweaking

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,7 @@ $(document).ready(() => {
     console.log("javascript loaded");
 })
 
-
+/*
 $(".btn btn-lg active").click(turnToHomePage);
 
 function turnToHomePage(e){
@@ -13,4 +13,101 @@ function turnToHomePage(e){
 	$(this).text('name');
 }
 
-$(".btn-home-search).click();
+$(".btn-home-search").click();
+*/
+$("#login").click(goToLogIn);
+$("#signup").click(goToSignUp);
+
+
+function goToLogIn(){
+	event.preventDefault();
+	console.log("login clicked");
+	$("div.login-box").replaceWith('<div class="login-box page-two">\
+			<div class="row">\
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">\
+					<img src="https://statewideguttercompany.com/wp-content/uploads/2012/07/logo-placeholder.jpg" width="100px" height="100px">\
+				</div>\
+			</div>\
+			<div class="row">\
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">\
+					<h1>hApp</h1>\
+				</div>\
+			</div>\
+			<form>\
+			<div class="row login-row">\
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">\
+					<input type="text" placeholder="USERNAME"></input>\
+				</div>\
+			</div>\
+			<div class="row login-row">\
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">\
+					<input type="text" placeholder="PASSWORD"></input>\
+				</div>\
+			</div>\
+			<div class="row login-row">\
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">\
+					<input type="checkbox">Remember Me</input>\
+				</div>\
+			</div>\
+			<div class="row login-row">\
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">\
+					<a href="home"><button type="button" class="btn btn-lg active btn-primary">LOG IN</button></a>\
+				</div>\
+			</div>\
+			</form>\
+			<div class="row login-row">\
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">\
+					<a href="#">Forgot Password</a>\
+				</div>\
+			</div>\
+			<div class="row login-row">\
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">\
+					<a href=""><--Back</a>\
+				</div>\
+			</div>\
+		</div>');
+}
+
+function goToSignUp(){
+	event.preventDefault();
+	console.log("signup clicked");
+	$("div.login-box").replaceWith('<div class="login-box page-two">\
+			<div class="row">\
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">\
+					<img src="https://statewideguttercompany.com/wp-content/uploads/2012/07/logo-placeholder.jpg" width="100px" height="100px">\
+				</div>\
+			</div>\
+			<div class="row">\
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">\
+					<h1>hApp</h1>\
+				</div>\
+			</div>\
+			<form>\
+			<div class="row login-row">\
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">\
+					<input type="text" placeholder="EMAIL"></input>\
+				</div>\
+			</div>\
+			<div class="row login-row">\
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">\
+					<input type="text" placeholder="USERNAME"></input>\
+				</div>\
+			</div>\
+			<div class="row login-row">\
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">\
+					<input type="text" placeholder="PASSWORD"></input>\
+				</div>\
+			</div>\
+			<div class="row login-row">\
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">\
+					<a href="home"><button type="button" class="btn btn-lg active btn-primary">SIGN UP</button></a>\
+				</div>\
+			</div>\
+			</form>\
+			<div class="row login-row">\
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">\
+					<a href=""><--Back</a>\
+				</div>\
+			</div>\
+		</div>');
+}
